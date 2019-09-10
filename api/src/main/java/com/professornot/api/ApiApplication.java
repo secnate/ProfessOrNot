5f1cbffc2ab068c64f61d49a1081ba904b38c0e8
Will Bootle <willbootle@me.com>
2019-09-10 12:24:59 -0400

    Setup the Auth0 demo, have it authenticating correctly, ready to integrate swagger and begin API development.

---
+++ api/src/main/java/com/professornot/api/ApiApplication.java
@@ -0,0 +1,20 @@
+package com.professornot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@SpringBootApplication
+@PropertySources({
+		@PropertySource("classpath:application.properties"),
+		@PropertySource("classpath:auth0.properties")
+})
+public class ApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+}
