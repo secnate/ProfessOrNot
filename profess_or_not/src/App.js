48fddecd2eb2739b5e21a64ad21d3697267ba68c
Nathan Pavlovsky <pavlovskynathan3141@gmail.com>
2019-10-19 21:38:07 -0400

    fixed the login button formatting

--- profess_or_not/src/App.js
+++ profess_or_not/src/App.js
@@ -72,7 +72,7 @@ import SearchIcon from "./search.png";
 
             <button className="loginLogoutButton"
                     onClick={clickLoginLogoutButton}>
-              {getLoginLogoutButtonText}
+              Login
             </button>
           </div>
 
