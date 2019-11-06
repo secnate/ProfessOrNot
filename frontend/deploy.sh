718929cad04b90c6c2ab196f47b2991d316045ce
Will Bootle <willbootle@me.com>
2019-11-06 17:29:48 -0500

    change to correct name

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 eval "$(ssh-agent -s)" # Start ssh-agent cache
-chmod 600 .travis/id_rsa # Allow read access to the private key
-ssh-add .travis/id_rsa # Add the private key to SSH
+chmod 600 .travis/deploy_key # Allow read access to the private key
+ssh-add .travis/deploy_key # Add the private key to SSH
 
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh root@$IP <<EOF
