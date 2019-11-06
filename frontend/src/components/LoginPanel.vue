f36e37f087c31bdf341bca96bca50cea13f2918c
Will Bootle <willbootle@me.com>
2019-11-06 12:15:18 -0500

    Profile popover working view but not edit #101

--- frontend/src/components/LoginPanel.vue
+++ frontend/src/components/LoginPanel.vue
@@ -1,39 +1,28 @@
 <template>
-<div>
-<div v-if="registerSelected" class="login-card">
-  <b-card
-    title="Register"
-    img-src="../assets/logotitle.png"
-    img-alt="Image"
-    img-top
-    style="max-width: 20rem;"
-    class="mb-2"
-  >
-    <Register />
-        <br>
-  <b-link v-on:click="toggleLogin" variant="primary"> Login </b-link>
-  </b-card>
-  </div>
-  <div v-else class="login-card">
-  <b-card
-    title="Login"
-    img-src="../assets/logotitle.png"
-    img-alt="Image"
-    img-top
-    style="max-width: 20rem;"
-    class="mb-2"
-  >
-    <Login />
-    <br>
-  <b-link v-on:click="toggleLogin" variant="primary"> Register </b-link>
-  </b-card>
-  </div>
+  <div class="login-card">
+    <b-card
+      :title="cardTitle"
+      img-src="../assets/logotitle.png"
+      img-alt="Image"
+      img-topb
+      style="max-width: 20rem;"
+      class="mb-2"
+    >
+      <div v-if="registerSelected">
+        <Register />
+      </div>
+      <div v-else>
+        <Login />
+      </div>
+      <br>
+      <b-link v-on:click="toggleLogin" variant="primary">{{switchButtonText}}</b-link>
+    </b-card>
   </div>
 </template>
 
 <script>
 import Login from "./Login.vue";
-import Register from "./Register.vue"
+import Register from "./Register.vue";
 export default {
   name: "LoginPanel",
   components: {
@@ -41,24 +30,31 @@ export default {
     Register
   },
   data() {
-      return {
-          registerSelected: false
-      }
+    return {
+      registerSelected: false
+    };
   },
   methods: {
-      toggleLogin: function() {
-          this.registerSelected = !this.registerSelected
-      }
+    toggleLogin: function() {
+      this.registerSelected = !this.registerSelected;
+    }
+  },
+  computed: {
+    cardTitle: function() {
+      return this.registerSelected ? "Register" : "Login"
+    },
+    switchButtonText: function() {
+      return this.registerSelected ? "Login" : "Register"
+    }
   }
-
 };
 </script>
 
 <style scoped>
 .login-card {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    height: 80vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 80vh;
 }
 </style>
