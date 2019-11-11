8edc7f5e0ff9e71e140b9caba011a18eb6e45009
Alex <brockthealex@gmail.com>
2019-11-11 13:06:27 -0500

    Added search bar, no functionality

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -1,25 +1,23 @@
 <template>
+  <div>
+    <Navbar />
+    <div>You're Logged In!</div>
     <div>
-        <Navbar/>
-        <div>
-        You're Logged In!
-        </div>
+      <input class="form-control" type="text" placeholder="Search" aria-label="Search" />
     </div>
+  </div>
 </template>
 
 <script>
-import Navbar from "./Navbar.vue"
+import Navbar from "./Navbar.vue";
 export default {
   name: "Main",
   components: {
-      Navbar,
+    Navbar
   },
   data() {
-      return {
-      }
+    return {};
   },
-  methods: {
-  }
-
+  methods: {}
 };
 </script>
