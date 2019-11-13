fcd06c3235fb9b2290035e21b8c7241f101068ad
Will Bootle <willbootle@me.com>
2019-11-13 17:05:01 -0500

    Quick fix, change POST review to respond with 201 CREATED instead of 200 OK

--- api/reviews/views.py
+++ api/reviews/views.py
@@ -2,6 +2,7 @@ from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
 from .models import Review
 from .serializers import ReviewSerializer
 from rest_framework.response import Response
+from rest_framework import status
 
 class ReviewList(ListCreateAPIView):
     serializer_class = ReviewSerializer
@@ -16,7 +17,7 @@ class ReviewList(ListCreateAPIView):
         print(type(request.data))
         serializer.is_valid(raise_exception=True)
         review = serializer.save(reviewer=request.user)
-        return Response(ReviewSerializer(review,context=self.get_serializer_context()).data)
+        return Response(ReviewSerializer(review,context=self.get_serializer_context()).data, status=status.HTTP_201_CREATED)
 class ReviewDetail(RetrieveAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
