907858af200d5cd197d32ffa3ecae059f73cabf0
Alex <brockthealex@gmail.com>
2019-11-14 12:53:41 -0500

    Update Main.vue

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -2,9 +2,10 @@
   <div>
     <Navbar />
     <div>You're Logged In!</div>
-    <div>
+    <!-- <div>
       <input class="form-control" type="text" placeholder="Search" aria-label="Search" />
-    </div>
+    </div> -->
+    
   </div>
 </template>
 
