f34a32df4dfb1a731e6a9f5af77b57f49be6e9f0
Alex <brockthealex@gmail.com>
2019-11-18 12:43:33 -0500

    Still working
    Adding functionality

--- frontend/src/components/Main.vue
+++ frontend/src/components/Main.vue
@@ -3,21 +3,40 @@
     <Navbar />
     <div>You're Logged In!</div>
     <div>
-      <input class="form-control" type="text" placeholder="Search" aria-label="Search" />
+      <b-form-input v-model="text" placeholder="Enter your name"></b-form-input>
+      <div class="mt-2">Value: {{ text }}</div>
+      <p>{{filteredLit}}</p>
     </div>
   </div>
 </template>
 
 <script>
 import Navbar from "./Navbar.vue";
+
 export default {
   name: "Main",
   components: {
     Navbar
   },
   data() {
-    return {};
+    return {
+      text: "",
+      List: [
+        "Duncan Buell",
+        "Veronica Wilson",
+        "James O'Rieley",
+        "John Doe",
+        "Test Professor1",
+        "Test Professor2"
+      ]
+    };
   },
-  methods: {}
+  methods: {
+    filteredList() {
+      return this.List.filter(List => {
+        return List.toLowerCase().includes(this.text.toLowerCase());
+      });
+    }
+  }
 };
 </script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,27 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import  router  from  './router'
-import  store  from  './store'
-import  Axios  from  'axios'
+import Vue from "vue";
+import App from "./App.vue";
+import BootstrapVue from "bootstrap-vue";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import router from "./router";
+import store from "./store";
+import Axios from "axios";
+import "vue-search-select/dist/VueSearchSelect.css";
 
-require('axios-base-url')('https://dev-api.professornot.com');
-Vue.config.productionTip = false
-Vue.prototype.$http  =  Axios;
-const accessToken  =  localStorage.getItem('token')
+require("axios-base-url")("https://dev-api.professornot.com");
+Vue.config.productionTip = false;
+Vue.prototype.$http = Axios;
+const accessToken = localStorage.getItem("token");
 
 if (accessToken) {
-    Vue.prototype.$http.defaults.headers.common['Authorization'] =  "Token " + accessToken
+  Vue.prototype.$http.defaults.headers.common["Authorization"] =
+    "Token " + accessToken;
 }
 // If token is present but user object is not, fetch the user
 if (store.getters.isAuthenticated && !store.getters.getUser.name) {
-  store.dispatch('fetch_user')
+  store.dispatch("fetch_user");
 }
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 new Vue({
   router,
   store,
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
