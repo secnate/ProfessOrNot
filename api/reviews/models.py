307916e0ad16a0c00a36527125962d4f25b3aac9
Will Bootle <willbootle@me.com>
2019-11-19 10:32:34 -0500

    Quick patch for course review avg

--- api/reviews/models.py
+++ api/reviews/models.py
@@ -8,7 +8,7 @@ class Review(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     reviewer = models.ForeignKey(User, related_name='reviews', on_delete=models.CASCADE, default=1)
     professor = models.ForeignKey(Professor, related_name='reviews', on_delete=models.CASCADE, default=1)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, default=1)
+    course = models.ForeignKey(Course, related_name='reviews', on_delete=models.CASCADE, default=1)
     rating = models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)], default=0)
     comment = models.CharField(max_length=255, default='')
     class Meta:
