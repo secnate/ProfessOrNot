cf146b61a75badfcde8078a9dfc409a2eed57ff0
Will Bootle <willbootle@me.com>
2019-11-25 11:06:11 -0500

    Patching auto deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-
 # Skip this command if you don't need to execute any additional commands after deploying.
 ssh root@$IP <<EOF
-  sh update.sh
+  rm -rf /var/www/html
+  mkdir /var/www/html
 EOF
+scp -r dist/* root@$IP:/var/www/html
