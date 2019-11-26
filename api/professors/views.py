71400b9f79092bf2178130b30680f1123f864301
Will Bootle <willbootle@me.com>
2019-11-26 12:13:48 -0500

    Fix #137 for Review, Professor, and Course. The unique together feature was not working correctly. Added a validator. Will return the following 400 status code error {     "non_field_errors": [         "The fields course, professor, reviewer must make a unique set."     ] }

--- api/professors/views.py
+++ api/professors/views.py
@@ -10,8 +10,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from django.http import Http404
 
+
 class ProfessorList(ListCreateAPIView):
     serializer_class = ProfessorSerializer
+
     def get_queryset(self):
         queryset = Professor.objects.all()
         queryset = queryset.filter(school__id=self.request.user.school_id)
@@ -19,17 +21,24 @@ class ProfessorList(ListCreateAPIView):
         if filter is not None:
             queryset = queryset.filter(name__icontains=filter)
         return queryset
+
     def post(self, request):
-        serializer = self.get_serializer(data=request.data)
+        new_professor_data = request.data
+        new_professor_data['school_id'] = request.user.school.id
+        serializer = self.get_serializer(data=new_professor_data)
         serializer.is_valid(raise_exception=True)
-        course = serializer.save(school=request.user.school)
-        return Response(ProfessorSerializer(course,context=self.get_serializer_context()).data, status=status.HTTP_201_CREATED)
+        course = serializer.save()
+        return Response(ProfessorSerializer(course, context=self.get_serializer_context()).data,
+                        status=status.HTTP_201_CREATED)
+
+
 class ProfessorDetail(APIView):
-    def get_object(self,pk):
+    def get_object(self, pk):
         try:
             return Professor.objects.get(pk=pk)
         except Professor.DoesNotExist:
             raise Http404
+
     def get(self, request, pk):
         professor = self.get_object(pk)
         professor_serialized = ProfessorSerializer(professor)
@@ -42,7 +51,7 @@ class ProfessorDetail(APIView):
                 courses.append(course)
         courses_serialized = CourseSerializer(courses, many=True)
         return Response({
-            "professor" : professor_serialized.data,
-            "courses" : courses_serialized.data,
-            "reviews" : reviews_serialized.data,
+            "professor": professor_serialized.data,
+            "courses": courses_serialized.data,
+            "reviews": reviews_serialized.data,
         })
--- api/reviews/views.py
+++ api/reviews/views.py
@@ -4,20 +4,27 @@ from .serializers import ReviewSerializer
 from rest_framework.response import Response
 from rest_framework import status
 
+
 class ReviewList(ListCreateAPIView):
     serializer_class = ReviewSerializer
+
     def get_queryset(self):
         queryset = Review.objects.all()
         schoolid = self.request.user.school_id
         if schoolid is not None:
             queryset = queryset.filter(professor__school__id=schoolid)
         return queryset
+
     def post(self, request):
-        serializer = self.get_serializer(data=request.data)
-        print(type(request.data))
+        new_review_data = request.data
+        new_review_data['reviewer_id'] = request.user.id
+        serializer = self.get_serializer(data=new_review_data)
         serializer.is_valid(raise_exception=True)
-        review = serializer.save(reviewer=request.user)
-        return Response(ReviewSerializer(review,context=self.get_serializer_context()).data, status=status.HTTP_201_CREATED)
+        review = serializer.save()
+        return Response(ReviewSerializer(review, context=self.get_serializer_context()).data,
+                        status=status.HTTP_201_CREATED)
+
+
 class ReviewDetail(RetrieveAPIView):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
