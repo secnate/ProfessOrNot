3b8dfd0aed530c604c725dec723890ae004fbc97
Will Bootle <willbootle@me.com>
2020-02-03 13:13:05 -0500

    Remove accidental files, fix module not found error.

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,14 +2,6 @@ import Vuex from 'vuex'
 import Vue from 'vue'
 import auth from './modules/auth'
 
-const {Builder, By, Key, util} = require("selenium-webdriver");
-async function example() {
-    let driver = await new Builder().forBrowser("firefox").build();
-    await driver.get("dev.professor.com");
-    await driver.findElement(By.name("q")).sendKeys("Selenium",Key.RETURN);
-}
-example();
-
 Vue.use(Vuex)
 
 export default new Vuex.Store({
