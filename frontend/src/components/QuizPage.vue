74810dadaad80946a700e73caf49650706e214cb
Nathan Pavlovsky <pavlovskynathan3141@gmail.com>
2020-04-03 21:04:28 -0400

    #208 Finished UI tweaks for quiz page

--- frontend/src/components/QuizPage.vue
+++ frontend/src/components/QuizPage.vue
@@ -8,7 +8,7 @@
 
     <br />
 
-    <h1>Learning Style Quiz</h1>
+    <h1 class="quiz_title">Learning Style Quiz</h1>
     <div v-if="status == 'load error'">Error retrieveing questions from server</div>
 
     <div v-if="status == 'loading'">
@@ -36,9 +36,29 @@
       >Submit</b-button>
     </div>
 
-    <div v-if="status == 'submitting'">Sending to server...</div>
-    <div v-if="status == 'submit error'">Error submitting to server</div>
-    <div v-if="status == 'submitted'">Successfully submitted to server</div>
+    <!-- Results of submission -->
+    <div v-if="status == 'submitting'">
+      <h1> Sending to Server... </h1>
+
+      <!-- This is a circulating spinner -->
+      <b-spinner 
+        variant="success" type="grow" 
+        style="margin-top: 150px; width: 55px; height: 55px; color: #7ac142;"
+      />
+    </div>
+    <div v-if="status == 'submit error'">
+      <h1> Error Submitting to Server. </h1>
+    </div>
+    <div v-if="status == 'submitted'">
+      <h1>  Successfully Submitted to Server! </h1>
+      
+      <!-- Filling in the circular spinner when submission is successful -->
+      <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+        <circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/>
+        <path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
+      </svg> 
+
+    </div>
   </div>
 </template>
 
@@ -69,6 +89,7 @@ export default {
     }
   }
 };
+
 </script>
 
 
@@ -79,4 +100,60 @@ export default {
   font-style: bold;
   color: red;
 }
+
+.quiz_title {
+  font-size: 35pt;
+  font-style: bold;
+}
+
+/* The following refers to the circle loader with checkmark CSS animation */
+.checkmark__circle {
+  stroke-dasharray: 166;
+  stroke-dashoffset: 166;
+  stroke-width: 2;
+  stroke-miterlimit: 10;
+  stroke: #7ac142;
+  fill: none;
+  animation: stroke 0.6s cubic-bezier(0.65, 0, 0.45, 1) forwards;
+}
+
+.checkmark {
+  width: 56px;
+  height: 56px;
+  border-radius: 50%;
+  display: block;
+  stroke-width: 2;
+  stroke: #fff;
+  stroke-miterlimit: 10;
+  margin: 10% auto;
+  box-shadow: inset 0px 0px 0px #7ac142;
+  animation: fill .4s ease-in-out .4s forwards, scale .3s ease-in-out .9s both;
+}
+
+.checkmark__check {
+  transform-origin: 50% 50%;
+  stroke-dasharray: 48;
+  stroke-dashoffset: 48;
+  animation: stroke 0.3s cubic-bezier(0.65, 0, 0.45, 1) 0.8s forwards;
+}
+
+@keyframes stroke {
+  100% {
+    stroke-dashoffset: 0;
+  }
+}
+@keyframes scale {
+  0%, 100% {
+    transform: none;
+  }
+  50% {
+    transform: scale3d(1.1, 1.1, 1);
+  }
+}
+@keyframes fill {
+  100% {
+    box-shadow: inset 0px 0px 0px 30px #7ac142;
+  }
+}
+
 </style>
