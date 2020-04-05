d32e46f7248f2fc6d528c41d36d7b1d9e8f95736
Will Bootle <willbootle@me.com>
2020-04-05 14:52:47 -0400

    Fix #206 weird redirect

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -12,9 +12,29 @@ export default {
     Main,
     LoginPage
   },
+  data() {
+    return {
+      appropriateComponent: this.$store.getters.isAuthenticated
+        ? "Main"
+        : "LoginPage"
+    };
+  },
   computed: {
-    appropriateComponent() {
-      return this.$store.getters.isAuthenticated ? "Main" : "LoginPage";
+    authStatus() {
+      return this.$store.getters.authStatus;
+    }
+  },
+  watch: {
+    authStatus(newVal, oldVal) {
+      if (newVal === "success") {
+        this.appropriateComponent = "Main";
+      }
+      if (newVal === "logout") {
+        this.appropriateComponent = "LoginPage";
+      }
+      if (newVal === "error") {
+        this.appropriateComponent = "LoginPage";
+      }
     }
   }
 };
